0f93f10e70451e6d17d785bedc76d2942f5990f6
Dave Arroyo <arroyo.daveil@gmail.com>
2017-03-01 10:12:33 +0800

    Add zoom in/out button

--- prospad.js
+++ prospad.js
@@ -20,9 +20,39 @@ var instanceConfig ={
 	tileHeight : 150,
 	initialZoomLevel : -1,
 	initialPositionFrame:{x:300,y:350},
+	callbackScope : this,
+    engineInstanceReadyCallback : this.onEngineInstanceReady,
 	mapDataPath : "assets/map.xml", // the path to the xml file that defines map data, required
-	assetsToLoad : ["assets/sprites/floor_N.png","assets/sprites/wallCorner_N.png","assets/sprites/wallStraight_W.png","assets/sprites/wallStraight_E.png"] // array of paths to the assets that are desired to be loaded by traviso, no need to use if assets are already loaded to PIXI cache, default null
+	assetsToLoad : ["assets/sprites/btn_zoomIn.png","assets/sprites/btn_zoomOut.png","assets/sprites/floor_N.png","assets/sprites/wallCorner_N.png","assets/sprites/wallStraight_W.png","assets/sprites/wallStraight_E.png"] // array of paths to the assets that are desired to be loaded by traviso, no need to use if assets are already loaded to PIXI cache, default null,
+
 };
 
 var engine = TRAVISO.getEngineInstance(instanceConfig);
 stage.addChild(engine);
+function onEngineInstanceReady(){
+	// create buttons
+	var btnZoomIn = new PIXI.Sprite.fromFrame("assets/sprites/btn_zoomIn.png");
+	stage.addChild(btnZoomIn);
+	var btnZoomOut = new PIXI.Sprite.fromFrame("assets/sprites/btn_zoomOut.png");
+	stage.addChild(btnZoomOut);
+
+	 // set positions
+	btnZoomIn.position.y = 8;
+	btnZoomOut.position.y = 8;
+	btnZoomIn.position.x = 8;
+	
+	btnZoomOut.position.x = btnZoomIn.position.x + btnZoomIn.width + 8;
+
+	btnZoomIn.interactive = btnZoomIn.buttonMode = true;
+	btnZoomOut.interactive = btnZoomOut.buttonMode = true;
+	// add click callbacks
+	btnZoomIn.click = btnZoomIn.tap = function(data)
+	{
+		engine.zoomIn();
+	};
+
+	btnZoomOut.click = btnZoomOut.tap = function(data)
+	{
+		engine.zoomOut();
+	};
+}
